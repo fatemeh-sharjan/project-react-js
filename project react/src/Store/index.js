91e9d1db608d86427062189ba5f1dbad2c391255
fatemehsharajn <sharjanfatemeh@gmail.com>
2025-02-01 01:08:42 +0330

    remove

--- project react/src/Store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartSliceReducer from "./Slice/CartSlice";
-
-const store=configureStore({
-    reducer:{
-        cart:CartSliceReducer
-    }
-})
-export default store
