118bc46d60307c4b75dc3dd0f1b4479c890d0614
fatemehsharajn <sharjanfatemeh@gmail.com>
2025-02-01 11:30:55 +0330

    Update index.jsx

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -37,7 +37,7 @@ export default function NavBar() {
         <Toolbar
           sx={{
             width: "80vw",
-            height: "51px",
+            height: "50px",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
